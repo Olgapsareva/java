5d22c830d342ebf136088fb1cb106848cc86b75e
olga_p <olgapsareva@yahoo.com>
2020-12-27 12:16:09 +0300

    upload homework 10 Exceeptions

---
+++ exceptions/MySizeArrayException.java
@@ -0,0 +1,13 @@
+package geekbrains.lesson10;
+
+public class MySizeArrayException extends Exception{
+
+    protected String errMessage;
+
+    public MySizeArrayException(String errMessage){
+        super(errMessage);
+
+    }
+
+
+}
