5f47f6a7e596c8d9e63ca070365581436a77fedb
olga_p <olgapsareva@yahoo.com>
2021-01-09 23:37:58 +0300

    updated contact & phoneBookAsMap

--- collections/Contact.java
+++ collections/Contact.java
@@ -1,5 +1,7 @@
 package geekbrains.lesson11;
 
+import java.util.Objects;
+
 public class Contact implements Comparable<Contact>{
     private String name;
     private int phoneNumber;
@@ -37,5 +39,17 @@ public class Contact implements Comparable<Contact>{
             return result;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contact contact = (Contact) o;
+        return phoneNumber == contact.phoneNumber &&
+                name.equals(contact.name);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, phoneNumber);
+    }
 }
--- collections/PhoneBookAsMap.java
+++ collections/PhoneBookAsMap.java
@@ -4,38 +4,42 @@ import java.util.*;
 
 public class PhoneBookAsMap {
 
-    private final Map<String, List<Integer>> contacts;
+    private final Map<String, List<Contact>> contacts;
 
     public PhoneBookAsMap(){
         contacts = new HashMap<>();
     }
 
-    public void add(String name, Integer tel){
-        List<Integer> listOfTelNumbers = contacts.get(name);
+    public void add(String name, Contact c){
+        List<Contact> listOfTelNumbers = contacts.get(name);
         if(listOfTelNumbers == null){
             listOfTelNumbers = new ArrayList<>();
         }
-        if(!listOfTelNumbers.contains(tel)){
-            listOfTelNumbers.add(tel);
+        if(!listOfTelNumbers.contains(c)){
+            listOfTelNumbers.add(c);
         }
         contacts.put(name, listOfTelNumbers);
 
     }
 
-    public List<Integer> get(String name){
+    public List<Contact> get(String name){
         return contacts.get(name);
 
     }
 
     public static void main(String[] args) {
         PhoneBookAsMap pb1 = new PhoneBookAsMap();
-        pb1.add("Ivanov", 123456);
-        pb1.add("Smirnov", 1234569876);
-        pb1.add("Sergeeva", 1234569876);
-        pb1.add("Ivanov", 12398765);
-        pb1.add("Ivanov", 123456);
+        Contact contact1 = new Contact("Ivanov", 123456);
+        Contact contact2 = new Contact("Sergeeva", 1234569876);
+        Contact contact3 = new Contact("Ivanov", 12398765);
+        Contact contact4 = new Contact("Ivanov", 123456);
+        pb1.add(contact1.getName(), contact1);
+        pb1.add(contact2.getName(), contact2);
+        pb1.add(contact3.getName(), contact3);
+        pb1.add(contact4.getName(), contact4);
 
-        System.out.println(pb1.get("Ivanov"));
+
+        System.out.println("поиск по фамилии " + pb1.get("Ivanov"));
 
         /*for(Map.Entry entry : pb1.contacts.entrySet()){
             System.out.println(entry);
